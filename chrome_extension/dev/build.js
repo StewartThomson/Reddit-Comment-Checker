d9e8af3aefba0e4f4006b21be01bbf5425cf9a99
Stewart Thomson <stewartthomson3@gmail.com>
2018-12-29 20:08:03 -0500

    - Added icons for extension

--- chrome_extension/dev/build.js
+++ chrome_extension/dev/build.js
@@ -1,6 +1,7 @@
 const fs = require('fs-extra');
 const concat = require('concat');
 const glob = require("glob");
+const path = require("path");
 
 (async function build() {
     const files = glob.sync("./src/*.js");
@@ -15,4 +16,15 @@ const glob = require("glob");
         './src/style.css',
         '../dist/style.css'
     );
+    const icons = glob.sync("./src/icons/*.png");
+    if (!fs.existsSync('../dist/icons')){
+        fs.mkdirSync('../dist/icons');
+    }
+    for(let icon of icons) {
+        let filename = path.basename(icon);
+        await fs.copyFile(
+            icon,
+            '../dist/icons/' + filename
+        )
+    }
 })();
